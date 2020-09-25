9a97e7b5be7e6ab52498c500a846e028cec33a97
uesmgn <ug.0114@icloud.com>
2020-09-25 10:15:19 +0900

    Update utils.py

--- src/models/backbone/utils.py
+++ src/models/backbone/utils.py
@@ -9,10 +9,10 @@ class Module(nn.Module):
       if isinstance(m, nn.Conv2d):
         nn.init.kaiming_normal_(m.weight, mode=mode, nonlinearity='relu')
         if m.bias is not None:
-          m.bias.data.zero_()
+            nn.init.zeros_(m.bias)
       elif isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.BatchNorm1d):
-        m.weight.data.fill_(1)
-        m.bias.data.zero_()
+        nn.init.constant_(m.weight, 1)
+        nn.init.zeros_(m.bias)
       elif isinstance(m, nn.Linear):
-        m.weight.data.normal_(0, 0.1)
-        m.bias.data.zero_()
+        nn.init.xavier_normal_(m.weight)
+        nn.init.zeros_(m.bias)
