6c85e345ec0ff12167469edb12265e9bcb47f63f
uesmgn <ug.0114@icloud.com>
2020-09-25 13:36:15 +0900

    Update validation.py

--- src/utils/validation.py
+++ src/utils/validation.py
@@ -1,7 +1,9 @@
 import os
+import re
+import numpy as np
 
 __all__ = [
-    'is_dir', 'is_hdf'
+    'is_dir', 'is_hdf', 'new_hdf', 'check_array'
 ]
 
 def yn_input(text):
@@ -41,3 +43,37 @@ def new_hdf(path):
             exit()
     else:
         return path
+
+def check_array(*args, allow_2d=True, sort=False, reverse=False,
+                unique=False, check_size=False, check_shape=False, dtype=None):
+    if len(args) > 1 and check_size:
+        if len(set([len(arr) for arr in args])) > 1:
+            raise ValueError('Input arrays must have same length')
+    if len(args) > 1 and check_shape:
+        if len(set([arr.shape for arr in args])) > 1:
+            raise ValueError('Input arrays must have same shape')
+    values = []
+    for i, arg in enumerate(args):
+        if not hasattr(arg, '__len__') or isinstance(arg, str):
+            raise ValueError('Input type must be array-like')
+        if len(arg) < 1:
+            raise ValueError('Input array must have elements')
+        arr = _cast_array(arg)
+        if arr.ndim is 1:
+            # 1-D array
+            # unique and sort operation only apply to 1-D array
+            if unique:
+                arr, index = np.unique(arr, return_index=True)
+                arr = arr[index.argsort()]
+            if sort:
+                arr = np.sort(arr)
+                if reverse:
+                    arr = arr[::-1]
+        elif allow_2d and arr.ndim is 2:
+            pass
+        else:
+            raise ValueError('Input type must be 1-D array')
+        if dtype is not None:
+            arr = arr.astype(_list_get(dtype, i) or dtype)
+        values.append(arr)
+    return tuple(values) if len(values) > 1 else values[0]
