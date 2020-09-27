91179b419ce4fd5dff34f88a6b0b0b4eac5cd82d
uesmgn <ug.0114@icloud.com>
2020-09-27 10:07:09 +0900

    Update run_iic.py

--- run_iic.py
+++ run_iic.py
@@ -10,10 +10,10 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 from attrdict import AttrDict
 from collections import defaultdict
-from sklearn import preprocessing
+import sklearn as sk
 import itertools
 
-import src.utils.validation as validation
+from src.utils import validation, preprocessing
 import src.models as models
 import src.datasets as datasets
 
@@ -37,7 +37,7 @@ def acronym(name):
 def plot_cm(cm, xlabels, ylabels, out):
     plt.figure(figsize=(8 * len(xlabels) / len(ylabels), 8))
     cmap = plt.get_cmap('Blues')
-    cm_norm = preprocessing.normalize(cm, axis=0, norm='l1')
+    cm_norm = sk.preprocessing.normalize(cm, axis=0, norm='l1')
     plt.imshow(cm_norm.T, interpolation='nearest', cmap=cmap, origin='lower')
     ax = plt.gca()
     ax.set_xticks(np.arange(len(xlabels)))
@@ -106,11 +106,10 @@ transform_fn = torchvision.transforms.Compose([
 ])
 
 perturb_fn = torchvision.transforms.Compose([
-    torchvision.transforms.ToPILImage(),
-    torchvision.transforms.RandomCrop((224 , 224 // random.uniform(1, 2))),
-    torchvision.transforms.Resize((224, 224)),
-    torchvision.transforms.ToTensor(),
-    torchvision.transforms.Lambda(lambda x: x + torch.randn_like(x) * 0.1),
+    torchvision.transforms.RandomChoice([
+        torchvision.transforms.Lambda(lambda x: x + torch.randn_like(x) * 0.1),
+        torchvision.transforms.Lambda(lambda x: preprocessing.gaussian_blur(x, 7, 10.)),
+    ]),
 ])
 
 parser = argparse.ArgumentParser()
