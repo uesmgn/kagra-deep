fbe751939f9d9421f1e4d0aabda95a895ee3bd6f
uesmgn <ug.0114@icloud.com>
2020-09-28 11:01:11 +0900

    Update samplers.py

--- src/data/samplers.py
+++ src/data/samplers.py
@@ -1,26 +1,22 @@
 import torch
-import torch.utils.data
+import torch.utils.data as data
 import torchvision
 from collections import defaultdict
 
 
-class BalancedDatasetSampler(torch.utils.data.sampler.Sampler):
+class BalancedDatasetSampler(data.sampler.Sampler):
 
     def __init__(self, dataset, callback_get_label, num_samples=None):
 
         self.indices = list(range(len(dataset)))
-
         self.callback_get_label = callback_get_label
-
         self.num_samples = num_samples or len(self.indices)
-
         label_to_count = defaultdict(lambda: 0)
         labels = []
         for idx in self.indices:
-            label = callback_get_label(idx)
+            label, _ = callback_get_label(idx)
             label_to_count[label] += 1
             labels.append(label)
-
         weights = [1. / label_to_count[labels[idx]] for idx in self.indices]
         self.weights = torch.DoubleTensor(weights)
 
