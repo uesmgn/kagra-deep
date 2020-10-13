543a53a6fd3f8e357b82d53ba4daba6cbd4f3ede
uesmgn <ug.0114@icloud.com>
2020-10-13 23:57:34 +0900

    Update samplers.py

--- src/data/samplers.py
+++ src/data/samplers.py
@@ -2,19 +2,21 @@ import torch
 import torch.utils.data as data
 import torchvision
 from collections import defaultdict
+import numbers
 
 __class__ = ["Balancer"]
 
 
 class Balancer(data.sampler.Sampler):
-    def __init__(self, dataset, expansion=1.0):
+    def __init__(self, dataset, expansion=1.0, max_num_samples=100000):
         self.dataset = dataset
-        if isinstance(expansion, float):
-            self.num_samples = int(len(dataset) * self.expansion)
-        elif isinstance(expansion, int):
+        if isinstance(expansion, numbers.Integral):
             self.num_samples = expansion
+        elif isinstance(expansion, numbers.Number):
+            self.num_samples = int(len(dataset) * expansion)
         else:
             raise ValueError("Invalid argument.")
+        self.num_samples = max(len(self.dataset), min(self.num_samples, max_num_samples))
         self.indices = list(range(len(self.dataset)))
         targets = self.dataset.targets
         counter = defaultdict(lambda: 0)
