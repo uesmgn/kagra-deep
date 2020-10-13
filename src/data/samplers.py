02fa95e387c0c46c7856184805d2d24c84d19f7c
uesmgn <ug.0114@icloud.com>
2020-10-13 23:42:21 +0900

    Update samplers.py

--- src/data/samplers.py
+++ src/data/samplers.py
@@ -3,40 +3,35 @@ import torch.utils.data as data
 import torchvision
 from collections import defaultdict
 
-__class__ = [
-    'Balancer'
-]
+__class__ = ["Balancer"]
+
 
 class Balancer(data.sampler.Sampler):
-
-    def __init__(self, dataset, expansion=10.):
-        self.__dataset = dataset
-        self.expansion = expansion
+    def __init__(self, dataset, expansion=5.0):
+        self.dataset = dataset
+        if isinstance(expansion, float):
+            self.num_samples = int(len(dataset) * self.expansion)
+        elif isinstance(expansion, int):
+            self.num_samples = expansion
+        else:
+            raise ValueError("Invalid argument.")
         self.init()
 
     def init(self):
-        self.indices = list(range(len(self.__dataset)))
-        self.num_samples = int(len(self.__dataset) * self.expansion)
-        targets = self.__dataset.targets
+        self.indices = list(range(len(self.dataset)))
+        targets = self.dataset.targets
         counter = defaultdict(lambda: 0)
         for idx in self.indices:
             label = targets[idx]
             counter[label] += 1
-        weights = [1. / counter[targets[idx]] for idx in self.indices]
+        weights = [1.0 / counter[targets[idx]] for idx in self.indices]
         self.weights = torch.DoubleTensor(weights)
 
-    @property
-    def dataset(self):
-        pass
-
-    @dataset.setter
-    def dataset(self, dataset):
-        self.__dataset = dataset
-        self.init()
-
     def __iter__(self):
-        return (self.indices[i] for i in torch.multinomial(
-            self.weights, self.num_samples, replacement=True))
+        return (
+            self.indices[i]
+            for i in torch.multinomial(self.weights, self.num_samples, replacement=True)
+        )
 
     def __len__(self):
         return self.num_samples
