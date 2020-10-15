9ceddc2f861c6aa360c4c3f12f7869820cafecfd
uesmgn <ug.0114@icloud.com>
2020-10-15 14:56:16 +0900

    Update functional.py

--- src/utils/functional.py
+++ src/utils/functional.py
@@ -4,7 +4,47 @@ from sklearn.metrics import precision_score, recall_score, f1_score, confusion_m
 import plotly.figure_factory as ff
 import numpy as np
 
-__all__ = ["to_device", "flatten", "tensordict"]
+__all__ = ["to_device", "multi_class_metrics", "flatten", "tensordict"]
+
+
+def multi_class_metrics(target, pred):
+    target = target.view(-1).detach().cpu().numpy()
+    pred = pred.view(-1).detach().cpu().numpy()
+
+    precision_micro = precision_score(target, pred, average="micro", zero_division=0)
+    precision_macro = precision_score(target, pred, average="macro", zero_division=0)
+    precision_weighted = precision_score(target, pred, average="weighted", zero_division=0)
+
+    recall_micro = recall_score(target, pred, average="micro", zero_division=0)
+    recall_macro = recall_score(target, pred, average="macro", zero_division=0)
+    recall_weighted = recall_score(target, pred, average="weighted", zero_division=0)
+
+    f1_micro = f1_score(target, pred, average="micro", zero_division=0)
+    f1_macro = f1_score(target, pred, average="macro", zero_division=0)
+    f1_weighted = f1_score(target, pred, average="weighted", zero_division=0)
+
+    labels = np.unique(target)
+    cm = confusion_matrix(target, pred, labels=labels)
+
+    fig = ff.create_annotated_heatmap(
+        cm, x=labels, y=labels, annotation_text=cm, colorscale="Blues", showscale=True
+    )
+    fig.update_xaxes(side="bottom")
+    fig.update_yaxes(autorange="reversed")
+
+    params = {
+        "precision_micro": precision_micro,
+        "precision_macro": precision_macro,
+        "precision_weighted": precision_weighted,
+        "recall_micro": recall_micro,
+        "recall_macro": recall_macro,
+        "recall_weighted": recall_weighted,
+        "f1_micro": f1_micro,
+        "f1_macro": f1_macro,
+        "f1_weighted": f1_weighted,
+        "cm": fig,
+    }
+    return params
 
 
 def to_device(device, *args):
@@ -34,16 +74,16 @@ class TensorDict(dict):
     def __init__(self):
         pass
 
-    def cat(self, d, dim=0):
+    def cat(self, d):
         if isinstance(d, abc.MutableMapping):
             for key, x in d.items():
                 assert torch.is_tensor(x)
-                x = x.detach().cpu()
+                x = x.unsqueeze(0).detach().cpu()
                 if key not in self:
                     self[key] = x
                 else:
                     old = self[key]
-                    self[key] = torch.cat([old, x], dim)
+                    self[key] = torch.cat([old, x], dim=0)
         else:
             raise ValueError("Invalid arguments.")
 
@@ -53,11 +93,18 @@ class TensorDict(dict):
             indices = list(range(x.ndim))
             if isinstance(keep_dim, int):
                 indices.pop(keep_dim)
-            for i in indices:
+                for i in indices:
+                    if mode == "mean":
+                        x = x.mean(i).unsqueeze(i)
+                    elif mode == "sum":
+                        x = x.sum(i).unsqueeze(i)
+                    else:
+                        raise ValueError("Invalid arguments.")
+            else:
                 if mode == "mean":
-                    x = x.mean(i).unsqueeze(i)
+                    x = x.mean()
                 elif mode == "sum":
-                    x = x.sum(i).unsqueeze(i)
+                    x = x.sum()
                 else:
                     raise ValueError("Invalid arguments.")
             new[key] = x.squeeze()
@@ -92,45 +139,6 @@ class TensorDict(dict):
             self.update(new)
             return self
         return new
-
-    def multi_class_metrics(self, true_label, pred_label, num_classes=20):
-        target = self[true_label].view(-1).detach().cpu().numpy()
-        pred = self[pred_label].view(-1).detach().cpu().numpy()
-
-        precision_micro = precision_score(target, pred, average="micro", zero_division=0)
-        precision_macro = precision_score(target, pred, average="macro", zero_division=0)
-        precision_weighted = precision_score(target, pred, average="weighted", zero_division=0)
-
-        recall_micro = recall_score(target, pred, average="micro", zero_division=0)
-        recall_macro = recall_score(target, pred, average="macro", zero_division=0)
-        recall_weighted = recall_score(target, pred, average="weighted", zero_division=0)
-
-        f1_micro = f1_score(target, pred, average="micro", zero_division=0)
-        f1_macro = f1_score(target, pred, average="macro", zero_division=0)
-        f1_weighted = f1_score(target, pred, average="weighted", zero_division=0)
-
-        labels = np.unique(target)
-        cm = confusion_matrix(target, pred, labels=labels)
-
-        fig = ff.create_annotated_heatmap(
-            cm, x=labels, y=labels, annotation_text=cm, colorscale="Blues", showscale=True
-        )
-        fig.update_xaxes(side="bottom")
-        fig.update_yaxes(autorange="reversed")
-
-        params = {
-            "precision_micro": precision_micro,
-            "precision_macro": precision_macro,
-            "precision_weighted": precision_weighted,
-            "recall_micro": recall_micro,
-            "recall_macro": recall_macro,
-            "recall_weighted": recall_weighted,
-            "f1_micro": f1_micro,
-            "f1_macro": f1_macro,
-            "f1_weighted": f1_weighted,
-            "cm": fig,
-        }
-        return params
 
 
 def tensordict():
