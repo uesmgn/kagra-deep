4d0fed60c41f2a40b7c25787a484686e34e46392
uesmgn <ug.0114@icloud.com>
2020-10-16 05:29:53 +0900

    Update vae.py

--- src/nn/models/vae.py
+++ src/nn/models/vae.py
@@ -4,6 +4,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 from ..utils import Module, Gaussian, Classifier, Reshape, ConvTranspose2dModule
 
 __all__ = ["M1", "M2"]
