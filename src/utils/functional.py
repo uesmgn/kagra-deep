086c6b878685e634f761891c9a281b80593de545
uesmgn <ug.0114@icloud.com>
2020-10-15 16:34:53 +0900

    Update functional.py

--- src/utils/functional.py
+++ src/utils/functional.py
@@ -1,6 +1,15 @@
 import torch
 from collections import abc
-from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.metrics import (
+    precision_score,
+    recall_score,
+    f1_score,
+    confusion_matrix,
+    mutual_info_score,
+    normalized_mutual_info_score,
+    adjusted_mutual_info_score,
+    adjusted_rand_score,
+)
 import plotly.figure_factory as ff
 import numpy as np
 
@@ -23,6 +32,11 @@ def multi_class_metrics(target, pred):
     f1_macro = f1_score(target, pred, average="macro", zero_division=0)
     f1_weighted = f1_score(target, pred, average="weighted", zero_division=0)
 
+    mis = mutual_info_score(target, pred)
+    nmis = normalized_mutual_info_score(target, pred)
+    amis = adjusted_mutual_info_score(target, pred)
+    ars = adjusted_rand_score(target, pred)
+
     labels = list(np.unique(target))
 
     matrix = confusion_matrix(target, pred, labels=labels)
@@ -43,6 +57,10 @@ def multi_class_metrics(target, pred):
         "f1_micro": f1_micro,
         "f1_macro": f1_macro,
         "f1_weighted": f1_weighted,
+        "mutual_info_score": mis,
+        "normalized_mutual_info_score": nmis,
+        "adjusted_mutual_info_score": amis,
+        "adjusted_rand_score": ars,
         "confusion_matrix": fig,
     }
     return params
