8a427d3df9b86e66a128114ab675a15bd6f6d40e
uesmgn <ug.0114@icloud.com>
2020-11-29 08:47:22 +0900

    Update samplers.py

--- src/data/samplers.py
+++ src/data/samplers.py
@@ -31,3 +31,24 @@ class Balancer(data.sampler.Sampler):
 
     def __len__(self):
         return self.num_samples
+
+
+class Upsampler(data.sampler.Sampler):
+    def __init__(self, dataset, num_samples=10000, max_num_samples=100000):
+        self.dataset = dataset
+        if isinstance(num_samples, numbers.Number):
+            self.num_samples = max(len(self.dataset), min(num_samples, max_num_samples))
+        else:
+            raise ValueError("Invalid argument.")
+        self.indices = list(range(len(self.dataset)))
+        weights = torch.ones(len(self.indices)) / len(self.indices)
+        self.weights = weights.double()
+
+    def __iter__(self):
+        return (
+            self.indices[i]
+            for i in torch.multinomial(self.weights, self.num_samples, replacement=True)
+        )
+
+    def __len__(self):
+        return self.num_samples
