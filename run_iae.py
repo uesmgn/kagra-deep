1265d83c01c66bf608c646920682b229428231f7
uesmgn <ug.0114@icloud.com>
2020-11-29 20:48:08 +0900

    Update run_iae.py

--- run_iae.py
+++ run_iae.py
@@ -63,7 +63,7 @@ def main(args):
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     if torch.cuda.is_available():
         torch.backends.cudnn.benchmark = True
-    model = IAE(dim_w=args.dim_w, dim_y=args.num_classes, dim_z=args.dim_z).to(device)
+    model = IAE2(dim_w=args.dim_w, dim_y=args.num_classes, dim_z=args.dim_z).to(device)
     optim = torch.optim.Adam(model.parameters(), lr=args.lr)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optim, T_0=2, T_mult=2)
 
