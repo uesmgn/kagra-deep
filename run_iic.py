a7d2a599d79ea5ad8c1d9a77e17ac7902879a669
uesmgn <ug.0114@icloud.com>
2020-12-17 02:00:38 +0900

    Update run_iic.py

--- run_iic.py
+++ run_iic.py
@@ -148,17 +148,17 @@ def main(args):
             for i, j in enumerate(random.sample(range(len(test_set)), 10)):
                 x, _ = test_set[j]
                 plt.subplot(10, args.num_ranking + 2, 12 * i + 1)
-                plt.imshow(x[0])
+                plt.imshow(x[0], cmap="gray")
                 plt.axis("off")
                 sim_indices = torch.argsort(y_simmat[j, :], descending=True)[1 : args.num_ranking + 1]
                 for n, m in enumerate(sim_indices):
                     plt.subplot(10, args.num_ranking + 2, 12 * i + 3 + n)
                     x, _ = test_set[m]
-                    plt.imshow(x[0])
+                    plt.imshow(x[0], cmap="gray")
                     plt.axis("off")
-                plt.tight_layout()
-                plt.savefig(f"simrank_e{epoch}.png")
-                plt.close()
+            plt.tight_layout()
+            plt.savefig(f"simrank_e{epoch}.png")
+            plt.close()
 
             # y_hyp = torch.mm(y_hyp, y_hyp.transpose(0, 1))
             # y_hyp = pca(y_hyp, 6)
