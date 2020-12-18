d090bcec6bcaaef02520b0d76fa33ea899f6c6c4
uesmgn <ug.0114@icloud.com>
2020-12-18 17:08:48 +0900

    Update run_m1_usl.py

--- run_m1_usl.py
+++ run_m1_usl.py
@@ -139,7 +139,7 @@ def main(args):
                         plt.close()
 
         if epoch % args.save_interval == 0:
-            torch.save(model.state_dict(), args.model_path)
+            torch.save(model.state_dict(), os.path.join(args.model_dir, "model_m1_usl.pt"))
 
 
 if __name__ == "__main__":
