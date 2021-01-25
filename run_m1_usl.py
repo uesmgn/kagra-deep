70cae7a99ea56cee031917cc818751c38e756885
uesmgn <ug.0114@icloud.com>
2021-01-25 13:16:03 +0900

    Update run_m1_usl.py

--- run_m1_usl.py
+++ run_m1_usl.py
@@ -195,20 +195,19 @@ def main(args):
                 size_cluster_i = ith_cluster_silhouette_values.shape[0]
                 y_upper = y_lower + size_cluster_i
                 c = cmap(i)
-                ax.fill_betweenx(np.arange(y_lower, y_upper), 0, ith_cluster_silhouette_values, facecolor=c, edgecolor=darken(c), alpha=0.7)
+                ax.fill_betweenx(np.arange(y_lower, y_upper), 0, ith_cluster_silhouette_values, facecolor=c, edgecolor=c, alpha=0.7)
 
                 # Label the silhouette plots with their cluster numbers at the middle
-                ax.text(-0.1, y_lower + 0.5 * size_cluster_i, targets[i])
+                ax.text(-0.1, y_lower + 0.5 * size_cluster_i, targets[i], color=c)
 
                 # Compute the new y_lower for next plot
-                y_lower = y_upper + 10  # 10 for the 0 samples
+                y_lower = y_upper + 50  # 10 for the 0 samples
 
                 ax.set_title("Silhouette coefficient for each cluster")
                 ax.set_xlabel("silhouette coefficient")
                 ax.set_ylabel("label")
 
                 ax.set_yticks([])  # Clear the yaxis labels / ticks
-                ax.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
 
             plt.tight_layout()
             plt.savefig(f"silhouette_e{epoch}.png", dpi=args.dpi)
