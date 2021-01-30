53840f9f2513d1b409a824101b2b906e1ba2be2f
uesmgn <ug.0114@icloud.com>
2021-01-30 16:30:50 +0900

    Update run_m1_usl.py

--- run_m1_usl.py
+++ run_m1_usl.py
@@ -72,7 +72,7 @@ def main(args):
 
     dataset = datasets.HDF5(args.dataset_root, transform_fn, target_transform_fn)
     train_set, test_set = copy.copy(dataset), dataset.sample(args.num_test_samples, stratify=dataset.targets)
-    train_set.transform = datasets.co(transform_fn, augment_fn, l=1)
+    train_set = datasets.co(train_set, transform_fn, augment_fn, l=1)
 
     def sampler_callback(ds, num_samples):
         return samplers.Upsampler(ds, num_samples)
